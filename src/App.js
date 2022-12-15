84746a9d4dd24e9d780b14e2f72786170468297f
tuhtinov.a <onlinely@yandex.ru>
2022-12-15 16:53:53 +0300

    15.12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import Layout from "./hoc/layout/layout"
-import Quiz from "./containers/Quiz/Quiz";
+import {Route, Switch} from 'react-router-dom'
+import Quiz from "./containers/Quiz/Quiz"
+import QuizList from "./containers/QuizList/QuizList"
+import Auth from "./containers/Auth/Auth"
+import QuizCreater from "./containers/QuizCreater/QuizCreater"
 
 class App extends Component {
   render() {
     return (
         <Layout>
-            <Quiz />
+            <Switch>
+                <Route path="/auth" component={Auth}/>
+                <Route path="/quiz-creator" component={QuizCreater}/>
+                <Route path="/quiz/:id" component={Quiz}/>
+                <Route path="/" component={QuizList}/>
+            </Switch>
         </Layout>
     )
   }
---
+++ src/containers/QuizCreater/QuizCreater.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class QuizCreater extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Quiz Creater</h1>
+            </div>
+        )
+    }
+}
